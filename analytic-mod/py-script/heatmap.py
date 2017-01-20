7827f62248d57d792b0e1be523601251bdddadab
David Allen <Davidallen417@gmail.com>
2017-01-19 19:17:36 -0500

    Custom colors on the heatmap
    Looks good, color bar's good

--- analytic-mod/py-script/heatmap.py
+++ analytic-mod/py-script/heatmap.py
@@ -7,6 +7,9 @@ import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits.axes_grid1.axes_divider import HBoxDivider
 import mpl_toolkits.axes_grid1.axes_size as Size
+from matplotlib import colors
+
+from cycler import cycler
 
 import os
 import csv
@@ -33,12 +36,14 @@ if __name__ == "__main__":
 
     arr1 = np.reshape(content, (23, 23))  # Make the data readable
 
+
     fig, (ax1) = plt.subplots()
-    cax = ax1.imshow(arr1, interpolation="nearest")
 
-    cbar = fig.colorbar(cax, ticks=[-1, 0, 1, 2, 3, 4])  # color bar
-
-    rect = 111  # subplot param for combined axes
+    cmap = colors.ListedColormap(['black','#00267F','#33CB64','#CFFA04', '#FFBE00', '#CC4632'])
+    bounds=[-1.5,-0.5, 0.5, 1.5, 2.5, 3.5, 4.5]
+    norm = colors.BoundaryNorm(bounds, cmap.N)
+    cax = ax1.imshow(arr1, interpolation="nearest", cmap=cmap, norm=norm)
+    cbar = fig.colorbar(cax, cmap=cmap, norm=norm, boundaries=bounds, ticks=[-1, 0, 1, 2, 3, 4])  # color bar
 
     for ax in [ax1]:
         ax.locator_params(nbins=4)
